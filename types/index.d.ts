1b7d38d5b35eb8aadd51ed3f3fc92db930d2cc3a
Wojtek PlanetHeros <wojtek@planetheroes.app>
2020-05-27 10:38:15 +0200

    Revert "Update index.d.ts"
    This reverts commit 963d6214221ecce877a16cea49be5995eb233983.

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,7 @@
 interface FacebookConnectPlugin {
     /** Get the version of Cordova running on the device. */
     /** Indicates that Cordova initialize successfully. */
-    getLoginStatus(onSuccess: (dict) => void,
+    getLoginStatus(onSuccess: (dict:Result) => void,
     onError: (err: string) => void):void;
 
     /** login to Facebbok 
@@ -12,21 +12,16 @@ interface FacebookConnectPlugin {
     onError: (err: string) => void):void;
     getAccessToken(onSuccess: (token:string) => void,
     onError: (err: string) => void):void;
-    logEvent(eName:string,permissions:[string],valueToSum,onSuccess: (dict) => void,
+    logEvent(permissions:[string]):void;
+    logPurchase(permissions:[string]):void;
+    checkHasCorrectPermissions(permissions:[string]):void;
+    logout(onSuccess: () => void, onError: (err: string) => void):void;
+    api(path:string,permissions:[string], onSuccess: (dict:Result) => void,
     onError: (err: string) => void):void;
-    logPurchase(value,currency,onSuccess: (dict) => void,
+    showDialog(options:ShowDialogOptions, onSuccess: (dict:Result) => void,
     onError: (err: string) => void):void;
-    checkHasCorrectPermissions(permissions:[string], onSuccess: (dict) => void,
-    onError: (err: string) => void):void;
-    logout(onSuccess: (response) => void, onError: (err: string) => void):void;
-    graphApi(path:string,permissions:[string], onSuccess: (dict) => void,
-    onError: (err: string) => void):void;
-    showDialog(options:ShowDialogOptions, onSuccess: (dict) => void,
-    onError: (err: string) => void):void;
-    getDeferredApplink(onSuccess: (dict) => void,
-    onError: (err: string) => void):void;
-    activateApp(onSuccess: (dict) => void,
-    onError: (err: string) => void):void;
+    getDeferredApplink(permissions:[string]):void;
+    activateApp(permissions:[string]):void;
 }
 
 interface ShowDialogOptions {
